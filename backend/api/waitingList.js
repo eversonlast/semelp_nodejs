5547ff9119545552695b0d05f0206905c48d8a18
eversonlast <33987857+eversonlast@users.noreply.github.com>
2022-08-20 21:21:11 -0300

    Comentário

--- backend/api/waitingList.js
+++ backend/api/waitingList.js
@@ -1,3 +1,7 @@
+/**********Módulo para Lista de espera em Mongo, melhor desempenho e controle
+ * 
+ */
+
 module.exports = app=>{ 
 
     const {existsOrError} = app.api.validation
@@ -9,16 +13,18 @@ module.exports = app=>{
         dataNasc: Date,
         idClass: Number,
         nomeModalidade: String,
+        dataDaMatricula: {type:Date}
 
     })
 
+    /***Salvar na lista de espera*******/
     const saveWaitingList = async(req, res)=>{
         
         const rowsVerifyUser = await waitingList.find({$and:[{idUser:req.params.id}, {idClass: req.query.class}]})
         const rowsVerifyUserClassBody = await waitingList.find({$and:[{idUser: req.body.idUser}, {idClass: req.body.idClass}]})
         const existsUser = await app.db('users').select('id', 'nome', 'dataNasc').first().where({id: req.body.idUser})
         const existsClass = await app.db('classes as c').join('modalities as m', 'm.id', 'c.idModality')
-                                .select('c.id', 'm.nomeModalidade', 'c.faixaEtaria').first().where({'c.id': req.body.idClass})
+                                .select('c.id', 'm.nomeModalidade', 'c.idadeMinima', 'c.idadeMaxima').first().where({'c.id': req.body.idClass})
         const ano = 1000*60*60*24*365;
         const idade = parseInt((Date.now() - existsUser.dataNasc)/ano)
 
@@ -30,13 +36,10 @@ module.exports = app=>{
         }catch(msg){
             return res.status(400).send(msg)
         }       
-            const idadeMinima = parseInt(existsClass.faixaEtaria.substring(0 , existsClass.faixaEtaria.toUpperCase().indexOf("À")))
-            const idadeMaxima = parseInt(existsClass.faixaEtaria.substring(existsClass.faixaEtaria.toUpperCase().indexOf("À")+2, existsClass.faixaEtaria.toUpperCase().indexOf("A")))
+            const idadeMinima = parseInt(existsClass.idadeMinima)
+            const idadeMaxima = parseInt(existsClass.idadeMaxima)
 
         try{
-            if(existsClass.faixaEtaria == "Acima de 60 anos" && idade < 60){
-                throw 'A idade está fora da faixa etária! Por favor verifique a Modalidade Escolhida!'
-            }
             if(!(idade > idadeMinima && idade < idadeMaxima)){
                 throw 'A idade está fora da faixa etária! Por favor verifique a Modalidade Escolhida'
             }
@@ -73,6 +76,9 @@ module.exports = app=>{
                     .then(waitList=> res.json(waitList))
         }
     }
+
+
+    /*******Pegar na lista de espera por turma********/
     const limit = 10
     const getWaitByClass = async(req, res)=>{
         const page = req.query.page || 1
@@ -81,6 +87,8 @@ module.exports = app=>{
                     .then(waitByClass=>res.json({data: waitByClass, limit: limit, count: count}))
     }
 
+
+    /*********Pega a contagem na lista de espera*******/
     const countUserWaitingList = async(req, res)=>{
         const dataInscricaoUser = await waitingList.find({idUser: req.body.idUser}, {_id: 0, dataInscricao: 1})
         await waitingList.aggregate([ {$match:{dataInscricao:{$gte:new Date(dataInscricaoUser)}}},
@@ -89,12 +97,18 @@ module.exports = app=>{
                     .then(countWait=>res.json(countWait))
     }
 
+
+    /********Pega na lista espera por usuário******/
     const getWaitingListByUser = async(req, res)=>{
         await waitingList.find({idUser: req.params.idUser}, 
                     {_id:0, dataInscricao: 1, idUser: 1, idClass: 1})
                     .then(listUser=>res.json(listUser))
     }
 
+
+    /*********Remove da lista de espera
+     * Precisa de controle de log, para isso.
+     */
     const removeWaitList = async(req, res)=>{
         await waitingList.deleteOne({idUser: req.params.idUser, 
                     idClass: req.query.idClass})
